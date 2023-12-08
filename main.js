d18ce6bc362a2b0b0fc07595d7d8fd87a1fbef1d
ha4rry <harrymawn@gmail.com>
2023-12-08 19:55:04 +0000

    v2.75
    big numbers work now. this may be the best update.

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 function isBinary(number) {
     // idk how to use regex, found this on google.
-    return /^[01]+$/.test(Number(number));
+    return /^[01]+$/.test(number);
 }
 
 function numberSystemToItsRealWordLol(numType) {
@@ -30,28 +30,40 @@ function convertFrom(numType) {
     errorText.innerText = ``
 
     
-    num = eval(numType).value;
 
-    if (numType == 'dec') {
-        hex.value = Number(num).toString(16);
-        bin.value = Number(num).toString(2);
-        oct.value = Number(num).toString(8);
-    }
-    else if (numType == 'hex') {
-        dec.value = parseInt(Number(num), 16);
-        bin.value = parseInt(Number(num), 16).toString(2);
-        oct.value = parseInt(Number(num), 16).toString(8);
-    }
-    else if (numType == 'bin') {
-        dec.value = parseInt(Number(num), 2);
-        hex.value = parseInt(Number(num), 2).toString(16);
-        oct.value = parseInt(Number(num), 2).toString(8);
-    }
-    else if (numType == 'oct') {
-        dec.value = parseInt(Number(num), 8);
-        hex.value = parseInt(Number(num), 8).toString(16);
-        bin.value = parseInt(Number(num), 8).toString(2);
-    }
+    
+    num = eval(numType).value;
+    try {
+        if (numType == 'dec') {
+            hex.value = BigInt(num).toString(16);
+            bin.value = BigInt(num).toString(2);
+            oct.value = BigInt(num).toString(8);
+        }
+        else if (numType == 'hex') {
+            dec.value = parseInt(num, 16);
+            bin.value = parseInt(num, 16).toString(2);
+            oct.value = parseInt(num, 16).toString(8);
+        }
+        else if (numType == 'bin') {
+            dec.value = parseInt(BigInt(num), 2);
+            hex.value = parseInt(BigInt(num), 2).toString(16);
+            oct.value = parseInt(BigInt(num), 2).toString(8);
+        }
+        else if (numType == 'oct') {
+            dec.value = parseInt(BigInt(num), 8);
+            hex.value = parseInt(BigInt(num), 8).toString(16);
+            bin.value = parseInt(BigInt(num), 8).toString(2);
+        }
+      } catch (SyntaxError) {
+
+        errorText.innerText = `ERROR: Invalid ${numberSystemToItsRealWordLol(numType)} value.`;
+        dec.value = 0;
+        hex.value = 0;
+        bin.value = 0;
+        oct.value = 0;
+      }
+      
+    
 
 
     
@@ -59,8 +71,11 @@ function convertFrom(numType) {
     // that if statement figures out how to convert from current numType input to DEC.
     
     
+    // if ( dec.value >= Math.pow(2,50) || hex.value >= Math.pow(2,50) || bin.value >= Math.pow(2,50) || oct.value >= Math.pow(2,50) ) {
+    //     errorText.innerText = `Warning: Numbers at this length may cause inaccuracies in conversion.`;
+    // }
 
-    if ( dec.value == "NaN" || hex.value == "NaN" || bin.value == "NaN" || oct.value == "NaN" || !(isBinary(bin.value)) ) {
+    if ( (dec.value == "NaN" || hex.value == "NaN" || bin.value == "NaN" || oct.value == "NaN" || !(isBinary(bin.value)) ) ){ //  
         errorText.innerText = `ERROR: Invalid ${numberSystemToItsRealWordLol(numType)} value.`;
         dec.value = 0;
         hex.value = 0;
@@ -70,3 +85,4 @@ function convertFrom(numType) {
 
 
 }
+
